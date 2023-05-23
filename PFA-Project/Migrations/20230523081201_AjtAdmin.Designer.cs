e3a17058d8efc353f6034913db132a9f630038be
JAADAR Ikram <ikramjaadar5@gmail.com>
2023-05-23 08:24:21 +0000

    AjtdbSet

--- PFA-Project/Migrations/20230523081201_AjtAdmin.Designer.cs
+++ PFA-Project/Migrations/20230523081201_AjtAdmin.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PFA_Project;
 
@@ -11,9 +12,10 @@ using PFA_Project;
 namespace PFA_Project.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230523081201_AjtAdmin")]
+    partial class AjtAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PFA-Project/Models/Admin.cs
+++ PFA-Project/Models/Admin.cs
@@ -1,8 +1,11 @@
-﻿namespace PFA_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PFA_Project.Models
 {
     public class Admin
     {
-       
+        [Key]
+        public int Id { get; set; }
         public string ? Login { get; set; }
         public string ?Password { get; set; }
     }
