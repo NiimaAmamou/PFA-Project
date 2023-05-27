c6f049c0fad40f4aba666983b59313f7346747ea
REHHALI Hanae <hanoureh@gmail.com>
2023-05-27 01:59:39 +0100

    DebutCrudEmployee

---
+++ PFA-Project/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PFA_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Ajouter()
+        {
+            ViewBag.EmpRoles=new SelectList(new[] { "Serveur", "Cuisinier", "Caissier", "Admin" });
+            return View();
+        }
+    }
+}
--- PFA-Project/Models/Employee.cs
+++ PFA-Project/Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PFA_Project.Models
 {
@@ -8,20 +9,33 @@ namespace PFA_Project.Models
      public class Employee
         {
             [Key]
-            public int Id { get; set; }
+            public int ?Id { get; set; }
+            [Required]
             public string Nom { get; set; }
+            [Required]
             public string Prenom { get; set; }
+            [Required]
             public DateTime Date_naissance { get; set; }
+            [Required]
             public string email { get; set; }
+            [Required]
             public double Salaire { get; set; }
+            [Required]
             public int Heure_Travail { get; set; }
-            public int Heure_Sup { get; set; }
-            public string Description { get; set; }
+        
+            public int ?Heure_Sup { get; set; }
+            public string ?Description { get; set; }
             public bool Disponibilite { get; set; }
+            [Required]
             public string Login { get; set; }
+            [Required]
             public string Password { get; set; }
-            public double RecetteServ { get; set; }
-            public int NbrExperience { get; set; }
+            [Required]
+            [NotMapped]
+            public string ConfirmationPass { get; set; }
+            public double ?RecetteServ { get; set; }
+            public int ?NbrExperience { get; set; }
+            [Required]
             public string Role { get; set; }
         }
     }
