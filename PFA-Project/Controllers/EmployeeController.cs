264813bb4ffdc1309490cea7276b922177084333
REHHALI Hanae <hanoureh@gmail.com>
2023-05-28 03:13:09 +0100

    CrudEmployee1

--- PFA-Project/Controllers/EmployeeController.cs
+++ PFA-Project/Controllers/EmployeeController.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using PFA_Project.Models;
 
 namespace PFA_Project.Controllers
 {
     public class EmployeeController : Controller
     {
+        public ApplicationContext db;
+        public EmployeeController(ApplicationContext db)
+        {
+            this.db = db;
+        }
         public IActionResult Ajouter()
         {
             ViewBag.EmpRoles=new SelectList(new[] { "Serveur", "Cuisinier", "Caissier", "Admin" });
             return View();
         }
+        [HttpPost]
+        [Route("Employee/Ajouter")]
+        public IActionResult Ajouter(String Role)
+        {
+            ViewBag.role=Role;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Employee/Ajouter",Order =1)]
+        public IActionResult Ajouter(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Ajouter");
+        }
+
     }
 }
